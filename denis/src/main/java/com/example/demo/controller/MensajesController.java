85b563e1013147e6dbdcc2af8f9d829f29a7de5b
Dani González <vito@growproexperience.com>
2022-09-22 10:41:03 +0200

    Editar mensajes

--- denis/src/main/java/com/example/demo/controller/MensajesController.java
+++ denis/src/main/java/com/example/demo/controller/MensajesController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -75,38 +76,42 @@ public class MensajesController  {
 
 	//Read an mensaje
 	@GetMapping("/{id}")
-	public ResponseEntity<?> read (@PathVariable(value = "id") Long mensajesId){
-		
+	public ModelAndView read (@PathVariable(value = "id") Long mensajesId){
+
 		Optional<Mensajes> oMensajes = mensajesservice.findById(mensajesId);
-		
+
 		if(!oMensajes.isPresent()) {
-			return ResponseEntity.notFound().build();
+            ModelAndView mav404 = new ModelAndView("404");
+            return mav404;
 		}
-		
-		return ResponseEntity.ok(oMensajes);
+
+		ModelAndView mav = new ModelAndView("editar");
+        mav.addObject("mensaje", oMensajes);
+        return mav;
 	}
 	
 	//Update an mensaje
 	
-	@PutMapping("/{id}")
-	public ResponseEntity<?> update(@RequestBody Mensajes mensajesDetails, @PathVariable(value = "id") Long mensajesId){
-		Optional<Mensajes> mensajes = mensajesservice.findById(mensajesId);
-		
-		if(!mensajes.isPresent()) {
-			return ResponseEntity.notFound().build();
+	@PostMapping("/{id}/editar")
+	public ModelAndView update(@RequestParam(value = "pais", required = false) String pais, @RequestParam(value = "mensajes", required = false) String mensajes, @PathVariable(value = "id") Long mensajesId){
+		Optional<Mensajes> mensaje = mensajesservice.findById(mensajesId);
+
+		if(!mensaje.isPresent()) {
+			ModelAndView mav404 = new ModelAndView("404");
+            return mav404;
 		}
-		
-		mensajes.get().setPais(mensajesDetails.getPais());
-		mensajes.get().setMensajes(mensajesDetails.getMensajes());
 
+		mensaje.get().setPais(pais);
+		mensaje.get().setMensajes(mensajes);
+		mensajesservice.save(mensaje.get());
 
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body(mensajesservice.save(mensajes.get()));
+        String urlInterna = "/mensajes/list";
+        return new ModelAndView("redirect:"+urlInterna);
 	}
 	
 	//Delete an Mensajes
 	
-	@DeleteMapping("/{id}")
+	@PostMapping("/{id}/borrar")
 	
 	public ResponseEntity<?> delete(@PathVariable(value = "id") Long mensajeId){
 		if (!mensajesservice.findById(mensajeId).isPresent()) {
